1b5d08c2dd961443a3a2d1187f485e8f96893bbb
Caleb Carlson <caleb.carlson@hpe.com>
2021-12-30 12:48:17 -0700

    Attempt to fit on dataset

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def main():
     }
     for col in numerical_cols:
         SPECS[col] = tf.TensorSpec(tf.TensorShape([]), tf.float32, name=col)
+
     pprint(SPECS)
 
     BATCH_SIZE = 32
@@ -59,7 +60,7 @@ def main():
     model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(LEARNING_RATE))
     model.summary()
 
-    history = model.fit(train_ds, epochs=EPOCHS)
+    history = model.fit(train_ds, epochs=EPOCHS, name=numerical_cols)
     # plt.plot(history.history['loss'])
     # plt.show()
 
