c9ebd72fb9fa66b0415b78cd557336c6af17c1a7
Caleb Carlson <caleb.carlson@hpe.com>
2022-01-05 11:19:45 -0700

    use function transpose()

--- main.py
+++ main.py
@@ -23,43 +23,6 @@ LEARNING_RATE = 0.001
 EPOCHS = 10
 BATCH_SIZE = 32
 
-
-def using_tfio_dataset():
-    # Fetch collection from MongoDB as training dataset
-    # dataset = tfio.experimental.mongodb.MongoDBIODataset(
-    #     uri=URI, database=DATABASE, collection=COLLECTION
-    # )
-    #
-    # tensor_specs = {
-    #     "feature": tf.TensorSpec(tf.TensorShape([]), tf.float32, name="TEMPERATURE_AT_SURFACE_KELVIN"),  # feature
-    #     "label": tf.TensorSpec(tf.TensorShape([]), tf.float32, name="TEMPERATURE_TROPOPAUSE_KELVIN")  # label
-    # }
-    # pprint(tensor_specs)
-    #
-    # dataset = dataset.map(
-    #     lambda x: tfio.experimental.serialization.decode_json(x, specs=tensor_specs)
-    # )
-
-    # Prepare a tuple of (features, label)
-    # dataset = dataset.map(lambda v: (v, v.pop("label")))
-    # dataset = dataset.batch(BATCH_SIZE)
-    # pprint(dataset)
-    #
-    # model = tf.keras.Sequential()
-    # model.add(tf.keras.Input(shape=(1,), name="feature"))
-    # model.add(tf.keras.layers.Dense(1, activation='relu'))
-    # model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(LEARNING_RATE))
-    # model.summary()
-    #
-    # history = model.fit(dataset, epochs=EPOCHS)
-    #
-    # res = model.evaluate(dataset)
-    # print("test loss, test acc:", res)
-    # plt.plot(history.history['loss'])
-    # plt.show()
-    pass
-
-
 def main():
     print("tensorflow version: {}".format(tf.__version__))
     print("tensorflow-io version: {}".format(tfio.__version__))
