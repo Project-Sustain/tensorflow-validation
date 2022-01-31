870674d113fafc23318bd8fc9f23ea776cc2437d
Caleb Carlson <ccarlson355@gmail.com>
2022-01-31 14:39:55 -0700

    Checkpoint

--- __init__.py
+++ __init__.py
@@ -12,12 +12,12 @@ from logging import info, error
 URI = "mongodb://localhost:27018"
 
 
-def validate_model(id, path, type, database, collection, label_field, validation_metric, feature_fields, gis_joins):
+def validate_model(job_id, path, model_type, database, collection, label_field, validation_metric, feature_fields, gis_join):
     client = MongoClient(URI)
     database = client[database]
     collection = database[collection]
 
-    documents = collection.find({'COUNTY_GISJOIN': 'G4802970'}, {'_id': 0, feature_fields[0]: 1, label_field: 1})
+    documents = collection.find({'COUNTY_GISJOIN': gis_join}, {'_id': 0, feature_fields[0]: 1, label_field: 1})
     features = []
     labels = []
     num_processed = 0
@@ -48,7 +48,7 @@ def validate_model(id, path, type, database, collection, label_field, validation
     info(f"normalized_labels shape: {normalized_labels.shape}")
 
     # Reload model
-    new_model = tf.keras.models.load_model(path)
+    new_model = tf.keras.models.load_model(f"{path}/{job_id}")
 
     # Check its architecture
     new_model.summary()
---
+++ testing/README.md
@@ -0,0 +1,3 @@
+# testing/
+
+Contains models and resources for testing tensorflow-validation.
