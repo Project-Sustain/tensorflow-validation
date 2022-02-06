d24aa49dab706a5200d9f83dca4acc8957ad95e5
Caleb Carlson <ccarlson355@gmail.com>
2022-02-06 13:03:18 -0700

    Imports...

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import tensorflow_io as tfio
 from pymongo import MongoClient
 from sklearn.preprocessing import normalize, MinMaxScaler
 
-from . import validate_model
+from tensorflow_validation import validate_model
 
 # MongoDB Stuff
 URI = "mongodb://lattice-100:27018/"
